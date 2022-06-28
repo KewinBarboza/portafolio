36e40e2d3631fce4decbf01710bb2708158024eb
kewinbarboza <kewinbarboza@gmail.com>
2022-06-28 19:52:34 -0400

    Updates

---
+++ data/proyectos.js
@@ -0,0 +1,45 @@
+export default [
+  {
+    "nombre": "Arca Rags",
+    "lenguajes": [
+      "PHP",
+      "Vue",
+      "HTML"
+    ],
+    "isPrivate": false,
+    "descripcion": "Catalogo online  para Arca-Rags",
+    "url": "http://www.aarcarags.com/#/",
+    "url_repositorio": "https://github.com/KewinBarboza/Arca-Rags",
+    "fork": false,
+    "imagen": "https://raw.githubusercontent.com/KewinBarboza/banner-proyectos/master/Arca-Rags.jpg"
+  },
+  {
+    "nombre": "Guía de GitHub",
+    "lenguajes": [
+      "JavaScript",
+      "HTML",
+      "CSS"
+    ],
+    "isPrivate": false,
+    "descripcion": "Guía sencilla de GuitHub",
+    "url": "http://KewinBarboza.github.io/Guia-de-GitHub",
+    "url_repositorio": "https://github.com/KewinBarboza/Guia-de-GitHub",
+    "fork": false,
+    "imagen": "https://raw.githubusercontent.com/KewinBarboza/banner-proyectos/master/Guia-de-GitHub.jpg"
+  },
+  {
+    "nombre": "portafolio",
+    "lenguajes": [
+      "Astro",
+      "JavaScript",
+      "HTML",
+      "CSS"
+    ],
+    "isPrivate": false,
+    "descripcion": "Portafolio",
+    "url": "https://kewinbarboza.github.io/Portafolio/",
+    "url_repositorio": "https://github.com/KewinBarboza/portafolio",
+    "fork": false,
+    "imagen": "https://raw.githubusercontent.com/KewinBarboza/banner-proyectos/master/portafolio.jpg"
+  }
+]
---
+++ data/redes_sociales.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    nombre: 'LinkedIn',
+    url: 'https://linkedin.com/in/kewinbarboza',
+    icon: 'linkedin'
+  },
+  {
+    nombre: 'GitHub',
+    url: 'https://github.com/KewinBarboza',
+    icon: 'github'
+  },
+]
