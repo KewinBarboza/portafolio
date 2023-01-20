802f10c8a0e2f7345b2c4738e28ce8390bf74755
kewinbarboza <kewinbarboza@gmail.com>
2023-01-20 10:53:00 -0400

    update info 2023

--- src/data/experiencia.js
+++ src/data/experiencia.js
@@ -1,60 +1,75 @@
 export default [
   {
-    nombre: 'Gimnasio Bodyfitness',
-    icono: 'pesas',
+    nombre: "Soluciones Laser",
+    icono: "empresa",
     proyectos: [
       {
-        titulo: 'Sistema para el administración y control de asistencia para el gimnasio.',
-        año: '2018-2020'
-      }
-    ]
+        titulo: "Desarrollador de aplicaciones móvil",
+        año: "2021 - 2023",
+      },
+    ],
   },
   {
-    nombre: 'I.U.T “Dr. Federico Rivero Palacio”',
-    icono: 'universidad',
+    nombre: "CareAndSafety",
+    icono: "trabajador",
     proyectos: [
       {
-        titulo: 'Creación de cuatro módulos para el sistema de gestión y control de notas del departamento de informática.',
-        año: '2016-2017'
-      }
-    ]
+        titulo: "Aplicación móvil para gestionar formularios",
+        año: "2022 - 2023",
+      },
+      {
+        titulo: "Sistema de Gestión de Seguridad y Salud en el trabajo",
+        año: "2020 - 2023",
+      },
+      {
+        titulo: "Landing page corporativa de la empresa",
+        año: "2020",
+      },
+    ],
   },
-
   {
-    nombre: 'Grupo Lockey',
-    icono: 'empresa',
+    nombre: "Grupo Lockey",
+    icono: "empresa",
     proyectos: [
       {
-        titulo: 'Sistemas de etiquetas para llevar el control de los productos del almacén',
-        año: '2018 - 2021'
+        titulo: "Sistema de amaestramiento de llaves",
+        año: "2021",
       },
       {
-        titulo: 'Sistema para el control de asistencia',
-        año: '2020'
+        titulo: "Sistema histórico de todos los datos de la empresa",
+        año: "2020 - 2021",
       },
       {
-        titulo: 'Sistema histórico de todos los datos de la empresa',
-        año: '2020 - 2021'
+        titulo: "Sistema para el control de asistencia",
+        año: "2020",
       },
       {
-        titulo: 'Sistema de amaestramiento de llaves',
-        año: '2021'
+        titulo:
+          "Sistemas de etiquetas para llevar el control de los productos del almacén",
+        año: "2018 - 2021",
       },
-    ]
+    ],
   },
-
   {
-    nombre: 'CareAndSafety',
-    icono: 'trabajador',
+    nombre: "Gimnasio Bodyfitness",
+    icono: "pesas",
     proyectos: [
       {
-        titulo: 'Landing page corporativa de la empresa',
-        año: '2020'
+        titulo:
+          "Sistema para el administración y control de asistencia para el gimnasio.",
+        año: "2018-2020",
       },
-      {
-        titulo: 'Sistema de Gestión de Seguridad y Salud en el trabajo',
-        año: '2020 - 2022'
-      },
-    ]
+    ],
   },
-]
+  {
+    nombre: "I.U.T “Dr. Federico Rivero Palacio”",
+    icono: "universidad",
+    proyectos: [
+      {
+        titulo:
+          "Creación de cuatro módulos para el sistema de gestión y control de notas del departamento de informática.",
+        año: "2016-2017",
+      },
+    ],
+  },
+];
