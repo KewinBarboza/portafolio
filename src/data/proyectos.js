f149d3dd39b55ea59e170a839dc7d2078f7c21a7
kewinbarboza <kewinbarboza@gmail.com>
2023-02-07 07:35:51 -0400

    add new proyect

--- src/data/proyectos.js
+++ src/data/proyectos.js
@@ -1,45 +1,42 @@
 export default [
   {
-    "nombre": "Arca Rags",
-    "lenguajes": [
-      "PHP",
-      "Vue",
-      "HTML"
-    ],
-    "isPrivate": false,
-    "descripcion": "catálogo online para Arca-Rags",
-    "url": "http://www.aarcarags.com/#/",
-    "url_repositorio": "https://github.com/KewinBarboza/Arca-Rags",
-    "imagen": "https://raw.githubusercontent.com/KewinBarboza/banner-proyectos/master/Arca-Rags.jpg"
-  },
-  {
-    "nombre": "Guía de GitHub",
-    "lenguajes": [
+    nombre: "Create Text",
+    lenguajes: [
+      "Astro",
       "JavaScript",
       "HTML",
-      "CSS"
+      "Tailwindcss",
+      "React.js",
+      "Cohere",
     ],
-    "isPrivate": false,
-    "descripcion": "Guía sencilla de GuitHub",
-    "url": "http://KewinBarboza.github.io/Guia-de-GitHub",
-    "url_repositorio": "https://github.com/KewinBarboza/Guia-de-GitHub",
-    "imagen": "https://raw.githubusercontent.com/KewinBarboza/banner-proyectos/master/Guia-de-GitHub.jpg"
+    isPrivate: false,
+    descripcion:
+      "App para crear descripción de productos y correos con inteligencia artificial",
+    url: "https://ctext.kewinbarboza.com/",
+    url_repositorio: "https://github.com/KewinBarboza/Create-Email",
+    imagen:
+      "https://raw.githubusercontent.com/KewinBarboza/banner-proyectos/master/create-text.webp",
+  },
+  {
+    nombre: "Guía de GitHub",
+    lenguajes: ["JavaScript", "HTML", "CSS"],
+    isPrivate: false,
+    descripcion: "Guía sencilla de GitHub",
+    url: "http://KewinBarboza.github.io/Guia-de-GitHub",
+    url_repositorio: "https://github.com/KewinBarboza/Guia-de-GitHub",
+    imagen:
+      "https://raw.githubusercontent.com/KewinBarboza/banner-proyectos/master/Guia-de-GitHub.jpg",
   },
 
   {
-    "nombre": "Sistema de Gestión de Seguridad",
-    "lenguajes": [
-      "JavaScript",
-      "HTML",
-      "CSS",
-      "MySQL",
-      "Bootstrap",
-      "Laravel"
-    ],
-    "isPrivate": true,
-    "descripcion": "Sistema de Gestión de Seguridad y Salud en el Trabajo para las empresas de Colombia",
-    "url": "https://careandsafety.co/",
-    "url_repositorio": "",
-    "imagen": "https://raw.githubusercontent.com/KewinBarboza/banner-proyectos/master/careandsafety.jpg"
+    nombre: "Sistema de Gestión de Seguridad",
+    lenguajes: ["JavaScript", "HTML", "CSS", "MySQL", "Bootstrap", "Laravel"],
+    isPrivate: true,
+    descripcion:
+      "Sistema de Gestión de Seguridad y Salud en el Trabajo para las empresas de Colombia",
+    url: "https://careandsafety.co/",
+    url_repositorio: "",
+    imagen:
+      "https://raw.githubusercontent.com/KewinBarboza/banner-proyectos/master/careandsafety.jpg",
   },
-]
+];
