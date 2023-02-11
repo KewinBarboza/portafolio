31598fabcee1fe6f7761b09375f42f60e49fce37
kewinbarboza <kewinbarboza@gmail.com>
2023-02-11 17:11:01 -0400

    add dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     container: {
       center: true,
       padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '15rem',
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "15rem",
       },
     },
     extend: {},
   },
   plugins: [],
-}
+};
