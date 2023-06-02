d13545038e6348469200a3c86ab3893f1eea70b7
desarrolloagil <desarrolloagilgit@gmail.com>
2023-06-02 09:39:51 -0400

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/static';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://kewinbarboza.github.io/'
+  site: 'https://kewinbarboza.github.io/',
+  adapter:vercel({
+    analytics:true
+  })
 });
