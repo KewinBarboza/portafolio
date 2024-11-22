8269b3b6b6200ec1b1058a8f299bb074d8dca3e8
KewinBarboza <kewinbarboza@gmail.com>
2024-11-22 12:40:36 -0400

    add imágenes experience

--- src/data/experiencia.js
+++ src/data/experiencia.js
@@ -7,17 +7,20 @@ export default [
       {
         titulo:
           "Desarrolle flujos de validaciones para realizar transferencias. Con estas validaciones los usuarios tienen una mayor seguridad a la hora de realizar transferías.",
-        año: "2023"
+        año: "2023",
+        img: "/experience/transferencias.svg"
       },
       {
         titulo:
           "Desarrolle la integración de los servicios de token digital para poder migrar a los usuarios de token físico para que puedan hacer transferencias más rápidas sin necesidad de ingresar manualmente un token físico en la aplicación móvil.",
-        año: "2022 - 2023"
+        año: "2022 - 2023",
+        img: "/experience/flujo-transferencias.svg"
       },
       {
         titulo:
           "Colabore en el desarrollo del módulo para consultar saldo a las tarjetas de crédito con Kore.ai. Este módulo sirve para que las personas puedan consultar más rápido su saldo por WhatsApp. ",
-        año: "2021 - 2022"
+        año: "2021 - 2022",
+        img: "/experience/saldo-whatsapp.svg"
       }
     ]
   },
@@ -29,17 +32,20 @@ export default [
       {
         titulo:
           "Diseñé y desarrolle la aplicación móvil para la gestión de formularios, permitiendo a los usuarios sin conexión a internet manejar la información de manera eficiente.",
-        año: "2022 - 2023"
+        año: "2022 - 2023",
+        img: "/experience/app-offline.svg"
       },
       {
         titulo:
           "Junto a un equipo, desarrollamos un Sistema de Gestión de Seguridad y Salud en el Trabajo. Esta innovación permitió a la empresa automatizar sus interacciones con los clientes, eliminando por completo el uso de papel y reemplazando los complicados formularios de Excel por un proceso más sencillo y eficiente para los usuarios.",
-        año: "2020 - 2023"
+        año: "2020 - 2023",
+        img: "/experience/app-careandsafety.svg"
       },
       {
         titulo:
           "Diseñé y Desarrolle la landing page corporativa de la empresa, proporcionando una plataforma para que los usuarios descubran las ventajas de colaborar con la empresa. Este desarrollo ha contribuido significativamente a aumentar la visibilidad y el reconocimiento de la marca.",
-        año: "2020"
+        año: "2020",
+        img: "/experience/landing-careandsafety.svg"
       }
     ]
   },
@@ -51,22 +57,26 @@ export default [
       {
         titulo:
           "Desarrolle e implementé un Sistema de Amaestramiento de Llaves que permitió a los trabajadores reducir el tiempo de preparación de pedidos de dos semanas a solo un día. Este cambio tuvo un impacto significativo en la productividad del departamento encargado.",
-        año: "2021"
+        año: "2021",
+        img: ""
       },
       {
         titulo:
           "Cree un Sistema Histórico de Datos para la empresa, permitiendo a los trabajadores realizar consultas exhaustivas de toda la información corporativa. Esta centralización de datos facilitó un acceso rápido y eficiente a la información para todos los departamentos.",
-        año: "2020 - 2021"
+        año: "2020 - 2021",
+        img: ""
       },
       {
         titulo:
           "Implementé un sistema de control de asistencia a través de un captahuellas, lo cual posibilitó al Departamento de Recursos Humanos registrar de manera precisa las horas de entrada y salida de los empleados. Esta innovación facilitó la gestión de la nómina y mejoró la eficiencia del departamento.",
-        año: "2020"
+        año: "2020",
+        img: ""
       },
       {
         titulo:
           "Desarrollé un sistema de etiquetado para optimizar la gestión de productos en el almacén. Esta solución mejoró significativamente la organización de los productos y permitió a los trabajadores aumentar su productividad.",
-        año: "2018 - 2021"
+        año: "2018 - 2021",
+        img: ""
       }
     ]
   },
@@ -78,7 +88,8 @@ export default [
       {
         titulo:
           "Desarrollé un sistema integral para la administración y control de asistencia en el gimnasio BodyFitness. Esta solución digital mejoró la eficiencia operativa y administrativa del gimnasio y proporcionó una experiencia de usuario optimizada.",
-        año: "2018-2020"
+        año: "2018-2020",
+        img: ""
       }
     ]
   },
@@ -90,7 +101,8 @@ export default [
       {
         titulo:
           "Desarrollé e integré un módulo de creación de documentos al sistema existente, lo que permitió al departamento aumentar su eficiencia en la creación de documentos en un 30%.",
-        año: "2016-2017"
+        año: "2016-2017",
+        img: ""
       }
     ]
   }
