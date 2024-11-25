db0281ad5ed606b5c09985e4fb72ec52f0c3fac0
KewinBarboza <kewinbarboza@gmail.com>
2024-11-25 11:26:51 -0400

    update timeline

--- src/data/experiencia.js
+++ src/data/experiencia.js
@@ -1,3 +1,5 @@
+import { bold, strong } from "../lib/html.ts"
+
 export default [
   {
     nombre: "Soluciones Laser",
@@ -5,20 +7,31 @@ export default [
     tiempo: "Actualmente",
     proyectos: [
       {
-        titulo:
-          "Desarrolle flujos de validaciones para realizar transferencias. Con estas validaciones los usuarios tienen una mayor seguridad a la hora de realizar transferías.",
+        titulo: `Desarrolle flujos de validaciones para ${bold(
+          "realizar transferencias"
+        )}. Con estas validaciones ${bold(
+          "los usuarios tienen una mayor seguridad"
+        )} a la hora de realizar transferías.`,
         año: "2023",
         img: "/experience/transferencias.svg"
       },
       {
-        titulo:
-          "Desarrolle la integración de los servicios de token digital para poder migrar a los usuarios de token físico para que puedan hacer transferencias más rápidas sin necesidad de ingresar manualmente un token físico en la aplicación móvil.",
+        titulo: `Desarrolle la integración de los ${bold(
+          "servicios de token digital"
+        )} para poder migrar a los usuarios de token físico para que puedan hacer ${bold(
+          "transferencias más rápidas"
+        )} sin necesidad de ${bold(
+          "ingresar manualmente un token físico"
+        )} en la aplicación móvil.`,
         año: "2022 - 2023",
         img: "/experience/flujo-transferencias.svg"
       },
       {
-        titulo:
-          "Colabore en el desarrollo del módulo para consultar saldo a las tarjetas de crédito con Kore.ai. Este módulo sirve para que las personas puedan consultar más rápido su saldo por WhatsApp. ",
+        titulo: `Colabore en el desarrollo del módulo para ${bold(
+          "consultar saldo a las tarjetas de crédito"
+        )} con Kore.ai. Este módulo sirve para que las personas puedan ${bold(
+          "consultar más rápido su saldo"
+        )} por WhatsApp. `,
         año: "2021 - 2022",
         img: "/experience/saldo-whatsapp.svg"
       }
@@ -30,20 +43,31 @@ export default [
     tiempo: "desde 2020 hasta 2023",
     proyectos: [
       {
-        titulo:
-          "Diseñé y desarrolle la aplicación móvil para la gestión de formularios, permitiendo a los usuarios sin conexión a internet manejar la información de manera eficiente.",
+        titulo: `Diseñé y desarrolle la aplicación móvil para la ${bold(
+          "gestión de formularios"
+        )}, permitiendo a los usuarios ${bold(
+          "sin conexión a internet"
+        )} manejar la información de manera eficiente.`,
         año: "2022 - 2023",
         img: "/experience/app-offline.svg"
       },
       {
-        titulo:
-          "Junto a un equipo, desarrollamos un Sistema de Gestión de Seguridad y Salud en el Trabajo. Esta innovación permitió a la empresa automatizar sus interacciones con los clientes, eliminando por completo el uso de papel y reemplazando los complicados formularios de Excel por un proceso más sencillo y eficiente para los usuarios.",
+        titulo: `Junto a un equipo, desarrollamos un ${bold(
+          " Sistema de Gestión de Seguridad y Salud en el Trabajo"
+        )}. Esta innovación permitió a la empresa ${bold(
+          "automatizar sus interacciones con los clientes"
+        )}, eliminando por completo el uso de papel y reemplazando los complicados ${bold(
+          "formularios de Excel por un proceso más sencillo y eficiente"
+        )} para los usuarios.`,
         año: "2020 - 2023",
         img: "/experience/app-careandsafety.svg"
       },
       {
-        titulo:
-          "Diseñé y Desarrolle la landing page corporativa de la empresa, proporcionando una plataforma para que los usuarios descubran las ventajas de colaborar con la empresa. Este desarrollo ha contribuido significativamente a aumentar la visibilidad y el reconocimiento de la marca.",
+        titulo: `Diseñé y Desarrolle la ${bold(
+          "landing page corporativa de la empresa"
+        )}, proporcionando una plataforma para que los usuarios descubran las ventajas de colaborar con la empresa. Este desarrollo ha contribuido significativamente a ${bold(
+          "aumentar la visibilidad y el reconocimiento de la marca."
+        )}`,
         año: "2020",
         img: "/experience/landing-careandsafety.svg"
       }
@@ -55,28 +79,42 @@ export default [
     tiempo: "desde 2018 hasta 2021",
     proyectos: [
       {
-        titulo:
-          "Desarrolle e implementé un Sistema de Amaestramiento de Llaves que permitió a los trabajadores reducir el tiempo de preparación de pedidos de dos semanas a solo un día. Este cambio tuvo un impacto significativo en la productividad del departamento encargado.",
+        titulo: `Desarrolle e implementé un sistema de ${bold(
+          "Amaestramiento de llaves"
+        )} que permitió a los trabajadores reducir el ${bold(
+          "tiempo de preparación de pedidos de dos semanas a solo un día"
+        )}. Este cambio tuvo un impacto significativo en la productividad del departamento encargado.`,
         año: "2021",
-        img: ""
+        img: "/experience/app-keys.svg"
       },
       {
-        titulo:
-          "Cree un Sistema Histórico de Datos para la empresa, permitiendo a los trabajadores realizar consultas exhaustivas de toda la información corporativa. Esta centralización de datos facilitó un acceso rápido y eficiente a la información para todos los departamentos.",
+        titulo: `Cree un Sistema Histórico de Datos para la empresa, permitiendo a los ${bold(
+          "trabajadores realizar consultas exhaustivas de toda la información corporativa"
+        )}. Esta centralización de datos facilitó un ${bold(
+          "acceso rápido y eficiente"
+        )} a la información para todos los departamentos.`,
         año: "2020 - 2021",
-        img: ""
+        img: "/experience/app-history.svg"
       },
       {
-        titulo:
-          "Implementé un sistema de control de asistencia a través de un captahuellas, lo cual posibilitó al Departamento de Recursos Humanos registrar de manera precisa las horas de entrada y salida de los empleados. Esta innovación facilitó la gestión de la nómina y mejoró la eficiencia del departamento.",
+        titulo: `Implementé un sistema de ${bold(
+          "control de asistencia a través de un captahuellas"
+        )}, lo cual posibilitó al Departamento de Recursos Humanos ${bold(
+          "registrar de manera precisa las horas de entrada y salida"
+        )} de los empleados. Esta innovación facilitó ${bold(
+          "la gestión de la nómina"
+        )} y mejoró la eficiencia del departamento.`,
         año: "2020",
-        img: ""
+        img: "/experience/app-asistencia.svg"
       },
       {
-        titulo:
-          "Desarrollé un sistema de etiquetado para optimizar la gestión de productos en el almacén. Esta solución mejoró significativamente la organización de los productos y permitió a los trabajadores aumentar su productividad.",
+        titulo: `Desarrollé un sistema de etiquetado para ${bold(
+          "optimizar la gestión de productos en el almacén"
+        )}. Esta solución mejoró significativamente la ${bold(
+          "organización de los productos"
+        )} y permitió a los ${bold("trabajadores aumentar su productividad")}.`,
         año: "2018 - 2021",
-        img: ""
+        img: "/experience/app-ticket.svg"
       }
     ]
   },
@@ -86,10 +124,13 @@ export default [
     tiempo: "desde 2018 hasta 2020",
     proyectos: [
       {
-        titulo:
-          "Desarrollé un sistema integral para la administración y control de asistencia en el gimnasio BodyFitness. Esta solución digital mejoró la eficiencia operativa y administrativa del gimnasio y proporcionó una experiencia de usuario optimizada.",
+        titulo: `Desarrollé junto a un grupo de trabajo un sistema integral para la ${bold(
+          "administración y control de asistencia"
+        )} en el gimnasio BodyFitness. Esta solución digital ${bold(
+          "mejoró la eficiencia operativa y administrativa"
+        )} del gimnasio y proporcionó una experiencia de usuario optimizada.`,
         año: "2018-2020",
-        img: ""
+        img: "/experience/app-asistencia-gym.svg"
       }
     ]
   },
@@ -99,10 +140,13 @@ export default [
     tiempo: "desde 2016 hasta 20217",
     proyectos: [
       {
-        titulo:
-          "Desarrollé e integré un módulo de creación de documentos al sistema existente, lo que permitió al departamento aumentar su eficiencia en la creación de documentos en un 30%.",
+        titulo: `Desarrollé e integré un módulo de ${bold(
+          "creación de documentos"
+        )} al sistema existente, lo que permitió al departamento ${bold(
+          "aumentar su eficiencia en la creación de documentos en un 30%."
+        )}`,
         año: "2016-2017",
-        img: ""
+        img: "/experience/app-document.svg"
       }
     ]
   }
